c278dfeb6ba1fb68e7a1be61fb8cbb49db37466c
niuhongkui <397208778@qq.com>
2018-09-27 23:12:36 +0800

    1

---
+++ Portal/Models/BankVModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal.Models
+{
+    public class BankVModel
+    {
+        public string Name { set; get; } 
+    }
+}
