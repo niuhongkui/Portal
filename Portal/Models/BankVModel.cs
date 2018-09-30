ea1289004cc2c6b660aa8bb45216f16be222872d
niuhongkui <397208778@qq.com>
2018-09-30 17:51:05 +0800

    3

--- Portal/Models/BankVModel.cs
+++ Portal/Models/BankVModel.cs
@@ -1,12 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Web;
+using Model.Entity;
 
 namespace Portal.Models
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class BankVModel
     {
-        public string Name { set; get; } 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="model"></param>
+        public BankVModel(Bank model)
+        {
+            BankName = model.Name;
+            BankUser = model.Operator.Name;
+            UserName = model.Operator.UserName;
+            UserPhone= model.Operator.Phone;
+            UserPwd = model.Operator.PassWord;
+            Id = model.Id;
+        }
+
+        /// <summary>
+        /// 银行名称
+        /// </summary>
+        public string BankName { set; get; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string BankUser { set; get; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string UserName { set; get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string UserPhone { set; get; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string UserPwd { set; get; }
+        /// <summary>
+        /// key
+        /// </summary>
+        public string Id { set; get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public Bank ToModel()
+        {
+            var bank=new Bank();
+            return bank;
+        }
+
     }
 }
