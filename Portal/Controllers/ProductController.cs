26f58a9920060cbd59120fec459aad60cecddf82
niuhongkui <397208778@qq.com>
2019-07-01 18:05:21 +0800

    页面

--- Portal/Controllers/ProductController.cs
+++ Portal/Controllers/ProductController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BLL;
+using Common;
 
 namespace Portal.Controllers
 {
     public class ProductController : BaseController
     {
+        private readonly ProductBLL _typeBll = new ProductBLL();
         // GET: Product
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parm"></param>
+        /// <returns></returns>
+        public JsonResult GetType(BaseParm parm)
+        {
+            parm.Id = UserInfo.StationId;
+            var json = _typeBll.List(parm);
+            return Json(json);
         }
     }
 }
