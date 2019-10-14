17f45de4f54622b0d4820edbed493d6110f241e7
niuhongkui <397208778@qq.com>
2019-10-14 09:39:58 +0800

    1

--- Portal/Controllers/SwiperController.cs
+++ Portal/Controllers/SwiperController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BLL;
+using Model;
 
 namespace Portal.Controllers
 {
     public class SwiperController : BaseController
     {
+        private SwiperBLL bll = new SwiperBLL();
         // GET: Swiper
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ApiMessage<bool> Edit(swiper model)
+        {
+            //var res=
+            return bll.Edit(model);
+        }
+
+        public ApiMessage<swiper> List(swiper model)
+        {
+            return bll.List(model);
         }
     }
 }
