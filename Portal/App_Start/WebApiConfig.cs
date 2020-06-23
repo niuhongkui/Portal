16c16b8806afbc5171f41053c0be28e3d6ba8f68
niuhongkui <397208778@qq.com>
2020-06-23 18:04:48 +0800

    webapi跨域

--- Portal/App_Start/WebApiConfig.cs
+++ Portal/App_Start/WebApiConfig.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Cors;
+using System.Configuration;
 
 namespace Portal
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public static class WebApiConfig
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
         public static void Register(HttpConfiguration config)
         {
+            var allowOrigins = ConfigurationManager.AppSettings["cors:allowOrigins"];
+            var allowHeaders = ConfigurationManager.AppSettings["cors:allowHeaders"];
+            var allowMethods = ConfigurationManager.AppSettings["cors:allowMethods"];
             // Web API 配置和服务
-
+            config.EnableCors(new EnableCorsAttribute(allowOrigins, allowHeaders, allowMethods));
             // Web API 路由
             config.MapHttpAttributeRoutes();
 
